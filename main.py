39472e3aa3522a902b6cb2b0848215f845699bc6
dr460n <pratham@dr460n.com>
2022-12-11 01:25:19 +0530

    inital and final commit (made for bakchodi.)

---
+++ main.py
@@ -0,0 +1,34 @@
+print("TRASH  (Basically your code whatever you are doing...)")
+print("TRASH  (Basically your code whatever you are doing...)")
+print("TRASH  (Basically your code whatever you are doing...)")
+print("TRASH  (Basically your code whatever you are doing...)")
+print("TRASH  (Basically your code whatever you are doing...)")
+print("TRASH  (Basically your code whatever you are doing...)")
+print("TRASH  (Basically your code whatever you are doing...)")
+print("TRASH  (Basically your code whatever you are doing...)")
+print("TRASH  (Basically your code whatever you are doing...)")
+print("TRASH  (Basically your code whatever you are doing...)")
+print("TRASH  (Basically your code whatever you are doing...)")
+
+
+#                                End of the code.
+#                                   By dr460n. 
+#                      ⠀⠀⠀⠀⠀⠀⠀⡼⡟⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#                      ⠀⠀⠀⠀⠀⠀⣸⢱⢸⠸⣆⡠⠤⠤⢀⣀⠀⠀⠀⢀⡤⠖⣦⠀⠀⠀⠀
+#                      ⠀⠀⠀⠀⠀⢠⠇⠈⠁⠀⠀⠀⠀⠀⠀⠀⠉⠳⢖⣡⠊⣇⡏⠀⠀⠀⠀
+#                      ⠀⠀⠀⠀⢀⠏⠀⠀⢀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠈⠉⢹⠁⠀⠀⠀⠀
+#                      ⠀⠀⠀⠀⡌⠀⠀⠀⠈⠳⠿⠽⡆⠀⢀⣴⣶⣶⣦⠀⠀⣾⠀⠀⠀⠀⠀
+#                      ⠀⠀⠀⣸⠁⠀⠀⠀⠀⠀⠀⠀⣭⢤⣚⠀⠉⠉⠁⠀⠀⣿⠀⠀⠀⠀⠀
+#                      ⠀⠀⢀⡇⠀⠀⠀⠀⠀⠸⣞⣂⢘⡖⢉⠀⢢⠀⠀⠀⠀⡿⠀⠀⠀⠀⠀
+#                      ⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⠀⣬⣉⢉⡛⠛⠋⠀⠀⠀⢠⡇⠀⠀⠀⠀⠀
+#                      ⠀⢠⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠸⡇⠀⠀⠀⠀⠀
+#                      ⠀⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣦⠀⠀⠀⠀
+#                      ⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⠀⠸⡆⠀⠀⠀
+#                      ⢸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢤⠖⠔⣡⠊⠀⠀⠀⠀⠀⠀⢿⠀⠀⠀
+#                      ⢸⠀⠀⠀⠀⠀⡆⠀⠀⠀⣠⠟⠀⠀⠀⣸⠃⠀⢰⢹⠀⠀⠀⢸⡟⢹⡆
+#                      ⣸⠀⠀⠀⠀⠀⡇⢀⡠⠞⠁⠀⠀⣠⠞⠁⠀⣠⠎⠸⣄⠀⠀⢸⡇⣸⠁
+#                      ⢹⠀⠀⠀⢀⡴⠟⠉⠀⠀⠀⢀⠞⠁⠀⠀⠠⡇⠀⠀⢸⠂⠀⢸⣧⡇⠀
+#                      ⢸⡄⠀⠀⠈⠀⠀⠀⠀⢀⠔⠁⠀⠀⠀⠀⠀⡇⠀⠀⣿⠀⠀⢸⡿⠀⠀
+#                      ⢸⢿⣄⠀⠀⠀⢀⣠⠖⠁⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⢻⡄⠀⣼⡇⠀⠀
+#                      ⠈⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠈⠁⠀⠀⠀⠀⠀
+#                           Meow, Now GTFO....
